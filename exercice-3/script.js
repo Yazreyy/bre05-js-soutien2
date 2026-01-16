3404344d89c4b61aedd9a57d49cc71d00a640de6
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-16 14:08:29 +0100

    exo 3

---
+++ exercice-3/script.js
@@ -0,0 +1,14 @@
+let users = [
+{pseudo : "Marlonn" , estEnLigne : true},
+{pseudo : "quentin" , estEnLigne : false},
+{pseudo : "Leon" , estEnLigne : true}
+];
+
+for (let i = 0 ; i < users.length ; i++){
+    if(users[i].estEnLigne === true){
+        console.log(users[i]);
+    	} else{
+        
+    }
+    i++
+}
